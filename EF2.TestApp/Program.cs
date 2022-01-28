33415e0b98d0fe9f04983f6e2438ff9184f06cee
Starinin Andrey <starininandrey@gmail.com>
2022-01-28 20:28:50 +0300

    Init

---
+++ EF2.TestApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using EF2.DB_Lib;
+
+namespace EF2.TestApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var db = new BookStoreDb();
+
+            foreach (var author in db.TabAuthors.ToList())
+            {
+                Console.WriteLine($"#{author.Id}: {author.LastName} {author.FirstName}");
+                foreach (var book in author.TabBooks.ToList())
+                {
+                    Console.WriteLine($"\t#{book.Id}: {book.Title}, {book.YearOfCreation}");
+                }
+            }
+        }
+    }
+}
